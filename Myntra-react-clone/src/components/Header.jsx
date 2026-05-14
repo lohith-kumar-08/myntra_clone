cb489a20b8a784f425bda1f15347c199f5ff22ab
Lohith Kumar A <lohithkumar@Lohiths-MacBook-Pro.local>
2026-05-14 14:16:55 +0530

    test

--- Myntra-react-clone/src/components/Header.jsx
+++ Myntra-react-clone/src/components/Header.jsx
@@ -15,8 +15,8 @@ const Header = () => {
                 <Link to=""><img className="myntra_home" src="images/myntra_logo.webp" alt="Myntra Home" /></Link>
             </div>
             <nav className="nav_bar">
-                <Link href="#">TEST6</Link>
-                <Link href="#">TEST6</Link>
+                <Link href="#">TEST7</Link>
+                <Link href="#">TEST7</Link>
                 <Link href="#">Kids</Link>
                 <Link href="#">Home & Living</Link>
                 <Link href="#">Beauty</Link>
